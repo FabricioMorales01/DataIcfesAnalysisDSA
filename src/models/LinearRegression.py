6ff23a8c2d38b8a301c9b650361a44a4a2ba0622
dayroncj <dayroncj@gmail.com>
2023-11-12 13:32:39 -0500

    Corrección del error al cambiar predictores entre experimentos

--- src/models/LinearRegression.py
+++ src/models/LinearRegression.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import LinearRegression
 
 # Definir la configuración para el entrenamiento del modelo.
 data_file = '../../data/processed/EXA_2022_1_Todos_Limp.csv'
-model = LinearRegression()
 predictors = ['ESTU_GRADO', 'COLE_COD_ICFES', 'COLE_COD_MCPIO', 'COLE_COD_DPTO', 'EXA_N_RTAS_CORR_CN', 'EXA_N_RTAS_CORR_CC', 'EXA_N_RTAS_CORR_MT']
 target = 'EXA_N_RTAS_CORR_LC'
 params = {'data_file': data_file, 'predictors': predictors, 'target': target, 'scaling': False, 'test_size': 0.33, 'random_state': 42}
@@ -15,19 +14,26 @@ metrics = ["neg_mean_squared_error", "neg_mean_absolute_error"]
 data = pd.read_csv(data_file, sep=',')
 
 # Entrenar el modelo y registrar experimento en MLFlow
+model = LinearRegression()
 train_model(model, data, params, metrics)
 
 # Escenario sin COLE_COD_ICFES
 predictors.remove('COLE_COD_ICFES')
-train_model(model, data, params, metrics)
+params['predictors'] = predictors
+model1 = LinearRegression()
+train_model(model1, data, params, metrics)
 
 # Escenario 2 considerando solamente el grado y los puntajes de los otros 3 instrumentos
 predictors = ['ESTU_GRADO', 'EXA_N_RTAS_CORR_CN', 'EXA_N_RTAS_CORR_CC', 'EXA_N_RTAS_CORR_MT']
-train_model(model, data, params, metrics)
+params['predictors'] = predictors
+model2 = LinearRegression()
+train_model(model2, data, params, metrics)
 
 # Escenario 3 considerando todas las variables numéricas menos COLE_COD_ICFES para evaluar el impacto del colegio en los resultados
 predictors = ['ESTU_GRADO', 'COLE_COD_MCPIO', 'COLE_COD_DPTO', 'EXA_N_RTAS_CORR_CN', 'EXA_N_RTAS_CORR_CC', 'EXA_N_RTAS_CORR_MT']
-train_model(model, data, params, metrics)
+params['predictors'] = predictors
+model3 = LinearRegression()
+train_model(model3, data, params, metrics)
 
 # Escenario 4 considerando todas las variables numéricas y categóricas
 categoricas_var = ["ESTU_GENERO", "COLE_NATURALEZA", "COLE_CALENDARIO", "COLE_JORNADA", "EXA_MODALIDAD"]
@@ -41,8 +47,11 @@ predictors = ['ESTU_GRADO',  'COLE_COD_ICFES', 'COLE_COD_MCPIO', 'COLE_COD_DPTO'
               'ESTU_GENERO_F', 'ESTU_GENERO_M', 'COLE_NATURALEZA_NO', 'COLE_NATURALEZA_O', 'COLE_CALENDARIO_A', 'COLE_JORNADA_COMPLETA', 'COLE_JORNADA_MAÑANA', 'COLE_JORNADA_TARDE', 'COLE_JORNADA_UNICA',
               'EXA_MODALIDAD_OFF', 'EXA_MODALIDAD_ON', 'EXA_MODALIDAD_PA', 'EXA_MODALIDAD_PAL']
 
-train_model(model, data_mod, params, metrics)
+params['predictors'] = predictors
+model4 = LinearRegression()
+train_model(model4, data_mod, params, metrics)
 
 # Escenario 5 escalando las variables numéricas
 params['scaling'] = True
-train_model(model, data_mod, params, metrics)
+model5 = LinearRegression()
+train_model(model5, data_mod, params, metrics)
