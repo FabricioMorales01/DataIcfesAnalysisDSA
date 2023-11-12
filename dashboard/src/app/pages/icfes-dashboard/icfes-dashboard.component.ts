3893241c959568a65daccffe42a81bc161c68f5f
fabricio.morales.t@gmail.com <fmorales@ionicsecurity.com>
2023-11-12 10:57:02 -0500

    Add initial dasboard

---
+++ dashboard/src/app/pages/icfes-dashboard/icfes-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-icfes-dashboard',
+  templateUrl: './icfes-dashboard.component.html',
+  styleUrls: ['./icfes-dashboard.component.scss']
+})
+export class IcfesDashboardComponent {
+
+}
